62279ee539e535b5283b446869e30a4b27daa3b2
yahia0123 <yahia0101@gmail.com>
2023-06-15 10:59:13 -0700

    15

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
-
-int main(void){
+#include <stdio.h
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,");
-	Return (0);
+	Return(0);
 }
