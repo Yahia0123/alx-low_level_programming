4151deded5ae877aea5b6edefacb9423c0e0c18a
yahia0123 <yahia0101@gmail.com>
2023-06-16 16:15:15 -0700

    m

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints all possible combinations of single-digit numbers
+ * 		Numbers must be separated by ,, followed by a space
+ *
+ * return: 0 (Success)
+*/
+
+int main(void)
+{
+	int ch = 1;
+
+	while (ch < 10)
+	{
+	putchar(ch);
+	putchar(',');
+	putchar(' ')
+	ch++;
+	}
+	putchar('\n');
+	return (0);
+}
