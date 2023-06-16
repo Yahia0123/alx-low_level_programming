cb8ec38bb60a0e3a43720417de7ce384be51ed3a
yahia0123 <yahia0101@gmail.com>
2023-06-16 15:57:50 -0700

    m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description:prints all the numbers of base 16 in lowercase
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int X = 1;
+
+	while (X < 16)
+	{
+	if (X >= 10)
+		putchar(a);
+		a++;
+	else
+		putchar(X + '0');
+	X++;
+	}
+	putchar('\n');
+	return (0);
+}
