79887a231694529b46b39f121f5fb28937904434
yahia0123 <yahia0101@gmail.com>
2023-06-20 07:52:16 -0700

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <main.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet, in lowercase
+ *
+ * Return: Always (0) (Success)
+*/
+
+int main(void)
+{
+	char ch = a;
+
+	putchar(ch);
+	ch ++;
+		putchar(\n);
+	return (0);
+}
