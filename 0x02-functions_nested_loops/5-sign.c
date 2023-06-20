99f0e04cee4b9ec929b77974e1737a0e3186f075
yahia0123 <yahia0101@gmail.com>
2023-06-20 11:58:46 -0700

    m

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,7 +17,7 @@ int print_sign(int n)
 	_putchar(43);
 	return (1);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		_putchar(48);
 	{	return (0);
@@ -25,5 +25,4 @@ int print_sign(int n)
 	{		_putchar(45);
 			return (-1);
 	}
-
 }
