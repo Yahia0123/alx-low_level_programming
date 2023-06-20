6ceac619f244556cde789860cfba8326be0ec3dc
yahia0123 <yahia0101@gmail.com>
2023-06-20 14:32:13 -0700

    m

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: X = sum of a+b
+*/
+
+int add(int a, int b)
+{
+	int X = a + b;
+       return (X);	
+}
