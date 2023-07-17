a3c0bdf097342f0561ea149bbe22dbc8c7ebd610
yahia0123 <yahia0101@gmail.com>
2023-07-17 15:43:42 -0700

    1

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,4 @@
-#include <stdio.h>
-
-/**
-  * main - Prints the file name of the program
-  *
-  * Return: Always zero
-  */
-int main(void)
-{
-	printf("%s\n", __FILE__);
-	return (0);
-}
+#ifndef _HEADER_
+#define _HEADER_
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
+#endif
