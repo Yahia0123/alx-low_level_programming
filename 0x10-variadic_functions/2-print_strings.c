54fc585dfb81daccc19f2f6b798780781b03ce9a
yahia0123 <yahiael46@gmail.com>
2023-10-13 09:18:56 -0700

    5

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,31 +1,41 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-
-/*
- * File: variadic_functions.h
- * Auth: Tolulope Fakunle
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0F-variadic_functions directory.
- */
-
+#include <stdio.h>
+#include "variadic_functions.h"
 #include <stdarg.h>
 
 /**
- * struct printer - A new struct type defining a printer.
- * @symbol: A symbol representing a data type.
- * @print: A function pointer to a function that prints
- *         a data type corresponding to symbol.
+ * print_strings - prints strings.
+ * @n: number of arguments
+ * @separator: character separator of numbers
+ * Return: Always 0.
  */
-typedef struct printer
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-	char *symbol;
-	void (*print)(va_list arg);
+	va_list string;
+	unsigned int i;
+	char *str;
 
-} printer_t;
+	if (separator == NULL)
+	{
+		separator = "";
+	}
 
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+	va_start(string, n);
 
-#endif
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(string, char *);
+		if (str == NULL)
+		{
+			printf("(nil)");
+			break;
+		}
+		printf("%s", str);
+		if (n == i + 1)
+		{
+			break;
+		}
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(string);
+}
